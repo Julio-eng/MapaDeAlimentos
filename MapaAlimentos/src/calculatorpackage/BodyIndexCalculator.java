940809d923ce9cb08d6320ba5e9554997f2479b0
julio <julio@DESKTOP-HQ3UFUD>
2021-03-20 18:44:40 -0300

    Creating class BodyIndexCalculator
    Co-authored-by: Valderson <valdersonpontes@gmail.com>

---
+++ MapaAlimentos/src/calculatorpackage/BodyIndexCalculator.java
@@ -0,0 +1,14 @@
+package calculatorpackage;
+
+public abstract class BodyIndexCalculator {
+	
+	protected double height;
+	
+	public double getheight() {
+		return height;
+	}
+
+	public void setAltura(double height) {
+		this.height = height;
+	}
+}
