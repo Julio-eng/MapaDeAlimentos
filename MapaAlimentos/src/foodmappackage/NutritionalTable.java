9c4cccc0f8de20c95173eaf834ea8e076f4cfa64
valde <valde@DESKTOP-QBOKR7T>
2021-03-20 18:41:13 -0300

    Creating the class NutritionalTable
    Co-authored-by: julio <julio0033@hotmail.com>

---
+++ MapaAlimentos/src/foodmappackage/NutritionalTable.java
@@ -0,0 +1,219 @@
+package foodmappackage;
+
+public class NutritionalTable {
+
+	private String name;
+	
+	//Energy
+	private double calorie;
+	
+	//Macronutrients
+	private double protein;
+	private double carbohydrate;
+	private double lipids;
+	
+	//Minerals
+	private double calcium;
+	private double iron;
+	private double fiber;
+	private double phosphor;
+	private double iodine;
+	private double magnesium;
+	private double potassium;
+	private double sodium;
+	private double zinc;
+	
+	//Vitamins
+	private double vitaminA;
+	private double vitaminB1;
+	private double vitaminB2;
+	private double vitaminB3;
+	private double vitaminB5;
+	private double vitaminB6;
+	private double vitaminB9;
+	private double vitaminB12;
+	private double vitaminC;
+	private double vitaminD;
+	private double vitaminE;
+	private double vitaminH;
+	private double vitaminK;
+	
+	
+	public double getCalorie() {
+		return calorie;
+	}
+	public void setCalorie(double calorie) {
+		this.calorie = calorie;
+	}
+	public double getProtein() {
+		return protein;
+	}
+	public void setProtein(double protein) {
+		this.protein = protein;
+	}
+	public double getCarbohydrate() {
+		return carbohydrate;
+	}
+	public void setCarbohydrate(double carbohydrate) {
+		this.carbohydrate = carbohydrate;
+	}
+	public double getLipids() {
+		return lipids;
+	}
+	public void setLipids(double lipids) {
+		this.lipids = lipids;
+	}
+	public double getCalcium() {
+		return calcium;
+	}
+	public void setCalcium(double calcium) {
+		this.calcium = calcium;
+	}
+	public double getIron() {
+		return iron;
+	}
+	public void setIron(double iron) {
+		this.iron = iron;
+	}
+	public double getFiber() {
+		return fiber;
+	}
+	public void setFiber(double fiber) {
+		this.fiber = fiber;
+	}
+	public double getPhosphor() {
+		return phosphor;
+	}
+	public void setPhosphor(double phosphor) {
+		this.phosphor = phosphor;
+	}
+	public double getIodine() {
+		return iodine;
+	}
+	public void setIodine(double iodine) {
+		this.iodine = iodine;
+	}
+	public double getMagnesium() {
+		return magnesium;
+	}
+	public void setMagnesium(double magnesium) {
+		this.magnesium = magnesium;
+	}
+	public double getPotassium() {
+		return potassium;
+	}
+	public void setPotassium(double potassium) {
+		this.potassium = potassium;
+	}
+	public double getSodium() {
+		return sodium;
+	}
+	public void setSodium(double sodium) {
+		this.sodium = sodium;
+	}
+	public double getZinc() {
+		return zinc;
+	}
+	public void setZinc(double zinc) {
+		this.zinc = zinc;
+	}
+	public double getVitaminA() {
+		return vitaminA;
+	}
+	public void setVitaminA(double vitaminA) {
+		this.vitaminA = vitaminA;
+	}
+	public double getVitaminB1() {
+		return vitaminB1;
+	}
+	public void setVitaminB1(double vitaminB1) {
+		this.vitaminB1 = vitaminB1;
+	}
+	public double getVitaminB2() {
+		return vitaminB2;
+	}
+	public void setVitaminB2(double vitaminB2) {
+		this.vitaminB2 = vitaminB2;
+	}
+	public double getVitaminB3() {
+		return vitaminB3;
+	}
+	public void setVitaminB3(double vitaminB3) {
+		this.vitaminB3 = vitaminB3;
+	}
+	public double getVitaminB5() {
+		return vitaminB5;
+	}
+	public void setVitaminB5(double vitaminB5) {
+		this.vitaminB5 = vitaminB5;
+	}
+	public double getVitaminB6() {
+		return vitaminB6;
+	}
+	public void setVitaminB6(double vitaminB6) {
+		this.vitaminB6 = vitaminB6;
+	}
+	public double getVitaminB9() {
+		return vitaminB9;
+	}
+	public void setVitaminB9(double vitaminB9) {
+		this.vitaminB9 = vitaminB9;
+	}
+	public double getVitaminB12() {
+		return vitaminB12;
+	}
+	public void setVitaminB12(double vitaminB12) {
+		this.vitaminB12 = vitaminB12;
+	}
+	public double getVitaminC() {
+		return vitaminC;
+	}
+	public void setVitaminC(double vitaminC) {
+		this.vitaminC = vitaminC;
+	}
+	public double getVitaminD() {
+		return vitaminD;
+	}
+	public void setVitaminD(double vitaminD) {
+		this.vitaminD = vitaminD;
+	}
+	public double getVitaminE() {
+		return vitaminE;
+	}
+	public void setVitaminE(double vitaminE) {
+		this.vitaminE = vitaminE;
+	}
+	public double getVitaminH() {
+		return vitaminH;
+	}
+	public void setVitaminH(double vitaminH) {
+		this.vitaminH = vitaminH;
+	}
+	public double getVitaminK() {
+		return vitaminK;
+	}
+	public void setVitaminK(double vitaminK) {
+		this.vitaminK = vitaminK;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	public String toString() {
+		return "NutritionalTable [name=" + name + ", calorie=" + calorie + ", protein=" + protein + ", carbohydrate="
+				+ carbohydrate + ", lipids=" + lipids + ", calcium=" + calcium + ", iron=" + iron + ", fiber=" + fiber
+				+ ", phosphor=" + phosphor + ", iodine=" + iodine + ", magnesium=" + magnesium + ", potassium="
+				+ potassium + ", sodium=" + sodium + ", zinc=" + zinc + ", vitaminA=" + vitaminA + ", vitaminB1="
+				+ vitaminB1 + ", vitaminB2=" + vitaminB2 + ", vitaminB3=" + vitaminB3 + ", vitaminB5=" + vitaminB5
+				+ ", vitaminB6=" + vitaminB6 + ", vitaminB9=" + vitaminB9 + ", vitaminB12=" + vitaminB12 + ", vitaminC="
+				+ vitaminC + ", vitaminD=" + vitaminD + ", vitaminE=" + vitaminE + ", vitaminH=" + vitaminH
+				+ ", vitaminK=" + vitaminK + "]";
+	}
+	
+	
+	
+}
