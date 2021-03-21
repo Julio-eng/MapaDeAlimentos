de1b5844ecc5a160766cc5064a7ade3f15da14ad
julio <julio@DESKTOP-HQ3UFUD>
2021-03-21 12:04:10 -0300

    Adding comments
    Co-authored-by: valderson <valdersonpontes@gmail.com>

--- MapaAlimentos/src/calculatorpackage/BAI.java
+++ MapaAlimentos/src/calculatorpackage/BAI.java
@@ -4,23 +4,28 @@ public final class BAI extends BodyIndexCalculator {
 	
 	private double hip;
 	
+	//metodo contrutuor 
 	public BAI(Double height, double hip) {
 		this.height = height;
 		this.hip = hip;
 	}
 	
+	//metodo construtor
 	public BAI() {
 		
 	}
 	
+	//metodo para printar o resultado do calculo do IAC
 	public void print() {
 		System.out.printf("IAC: %.2f", baiCalculation());	
 	}
 	
+	//metodo para calcular o IAC
 	public double baiCalculation() {
 		return this.hip/(this.height*this.height);
 	}
 	
+	//getters and setters
 	public double gethip() {
 		return hip;
 	}
