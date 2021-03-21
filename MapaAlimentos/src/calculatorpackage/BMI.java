cfa728b32b8a39aab6d2fbcf069b4de30a99b63c
julio <julio@DESKTOP-HQ3UFUD>
2021-03-21 12:04:18 -0300

    Adding comments
    Co-authored-by: valderson <valdersonpontes@gmail.com>

--- MapaAlimentos/src/calculatorpackage/BMI.java
+++ MapaAlimentos/src/calculatorpackage/BMI.java
@@ -4,23 +4,28 @@ public final class BMI extends BodyIndexCalculator {
 	
 	private double weight;
 	
+	//metodo contrutuor
 	public BMI(Double height, double weight) {
 		this.height = height;
 		this.weight = weight;
 	}
 	
+	//metodo contrutuor
 	public BMI() {
 		
 	}
 	
+	//metodo para printar o resultado do calculo do IMC
 	public void print() {
 		System.out.printf("IMC: %.2f", bmiCalculation());	
 	}
 	
+	//metodo para calcular o IMC
 	public double bmiCalculation() {
 		return this.weight/(this.height*this.height);
 	}
 	
+	//getters and setters
 	public double getPeso() {
 		return weight;
 	}
