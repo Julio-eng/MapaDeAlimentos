d96fc6fb4f97cb4731f7ecdf3a562370f415b20c
julio <julio@DESKTOP-HQ3UFUD>
2021-03-21 12:04:27 -0300

    Adding comments
    Co-authored-by: valderson <valdersonpontes@gmail.com>

--- MapaAlimentos/src/calculatorpackage/BodyIndexCalculator.java
+++ MapaAlimentos/src/calculatorpackage/BodyIndexCalculator.java
@@ -2,8 +2,10 @@ package calculatorpackage;
 
 public abstract class BodyIndexCalculator {
 	
+	//classe abstrata com o atributo height, compartilhado com a classe BMI e BAI
 	protected double height;
 	
+	///getters and setters
 	public double getheight() {
 		return height;
 	}
