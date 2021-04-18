f22078931f19b419d2043409c2e7b9a99b8edf46
julio <julio@DESKTOP-HQ3UFUD>
2021-04-18 00:59:00 -0300

    Adding informations to the attributes of Disease obejcts
    Co-authored-by: valderson <valdersonpontes@gmail.com>

--- MapaAlimentos/src/control/DiseaseControl.java
+++ MapaAlimentos/src/control/DiseaseControl.java
@@ -10,14 +10,21 @@ public class DiseaseControl {
 	public DiseaseControl () {
 		
 		lactoseIntolerant.setName("Intolerancia a lactose");
-		lactoseIntolerant.setDiseaseInformation("");
-		lactoseIntolerant.setSymptoms("");
-		lactoseIntolerant.setAvoidAliments("");
+		lactoseIntolerant.setDiseaseInformation("incapacidade total ou parcial do organismo de produzir a lactase, uma"
+				+ " enzima que quebra a lactose, o acucar dos produtos lacteos. Com isso, o composto se acumula no"
+				+ " intestino e e fermentado pelas bacterias que vivem ali, provocando mal-estar");
+		lactoseIntolerant.setSymptoms("Dor e inchaço abdominal,  diarreia,  gases,  azia,  nausea e dor de cabeça ");
+		lactoseIntolerant.setAvoidAliments("Alimentos industrializados, embutidos e processados e leite de origem animal"
+				+ " e todos os seus derivados");
 		
 		celiac.setName("Celiaca");
-		celiac.setDiseaseInformation("");
-		celiac.setSymptoms("");
-		celiac.setAvoidAliments("");
+		celiac.setDiseaseInformation("A doença celiaca ocorre quando as celulas de defesa imunologica agridem as celulas"
+				+ " do organismo, causando um processo inflamatorio. E uma inflamacao e provocada pelo gluten, proteina"
+				+ " presente no trigo, cevada e centeio");
+		celiac.setSymptoms("Diarreia, prisao de ventre, perda de peso, anemia, sensacao de estufamento, colica e "
+				+ "desconforto abdominal");
+		celiac.setAvoidAliments("Alimentos que possuem glutem, uma proteina presente nos graos de trigo, cevada, centeio "
+				+ "e espelta");
 		
 	}
 
