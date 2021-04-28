a6e807265691dce7f2ae1a42e8fbe05c2b8ea8e9
julio <julio@DESKTOP-HQ3UFUD>
2021-04-28 11:10:49 -0300

    adding new test
    Co-authored-by: valderson <valdersonpontes@gmail.com>

--- MapaAlimentos/src/testpackage/BMITest.java
+++ MapaAlimentos/src/testpackage/BMITest.java
@@ -4,15 +4,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-import model.BAI;
+import model.BMI;
 
 class BMITest {
 	
-	BAI objectBMI = new BAI(1.6, 60);
+	BMI objectBMI = new BMI(1.6, 60);
 
 	@Test
 	void testBmiCalculation() {
-		assertEquals(23.44, Double.parseDouble(objectBMI.baiCalculation()));
+		assertEquals(23.44, Double.parseDouble(objectBMI.bmiCalculation()));
+		assertNotEquals(23.4, Double.parseDouble(objectBMI.bmiCalculation()));
 	}
 
 }
