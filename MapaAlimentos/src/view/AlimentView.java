bcafe4daaad7cc71dc7ab8da54cde816dd83576a
julio <julio@DESKTOP-HQ3UFUD>
2021-04-28 11:08:06 -0300

    Changing javadoc
    Co-authored-by: valderson <valdersonpontes@gmail.com>

--- MapaAlimentos/src/view/AlimentView.java
+++ MapaAlimentos/src/view/AlimentView.java
@@ -27,8 +27,8 @@ import model.NutritionalTable;
  * CRUD of the NutritionalTable type aliments
  * Display of the graphic interface related to the list of aliments
  * Frame, panels, labels, imageIcons, buttons, textArea, textField
- * @author Julio and Valderson
- * @version 3.0 (Apr 2021)
+ * @author Julio Cesar and Valderson
+ * 
  */
 
 public class AlimentView implements ListSelectionListener, ActionListener {
